de08ecf596b503cdd9e43e2432d560d7df99f51f
milliewang <slwangpwa@gmail.com>
2023-07-17 18:24:20 +0800

    fixed valid react child err

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 import { download } from '../assets';
 import { downloadImage } from '../utils';
 
-const Card = ( _id, name, prompt, photo) => {
+const Card = ({_id, name, prompt, photo}) => {
   return (
     <div className='rounded-xl group relative shadow-card hover:shadow-cardhover card'>
       <img 
